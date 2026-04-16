ce5c3cb0bdb6666a07ecf1bbd321c22e5db1e1f6
“manar <albreikimanar@gmail.com>
2026-04-16 15:09:07 +0400

    add loops tasks

---
+++ solution07/project01/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace project01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region while
+            //Console.WriteLine("please enter any number");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //int attempts = 0;
+
+            //Random rnd = new Random();
+
+            //while (num!=  rnd.Next(0, 10))
+            //{
+            //    attempts++;
+            //    if (attempts < 3)
+            //    {
+            //        Console.WriteLine("Incorrect number");
+            //        Console.WriteLine("please enter any number");
+            //        num = Convert.ToInt32(Console.ReadLine()); 
+            //    } else
+            //    {
+            //        Console.WriteLine("you try too many times");
+            //        break;
+            //    }
+            //}
+            #endregion
+
+
+            #region String
+            //Console.WriteLine("enter the word");
+            //string x = Console.ReadLine();
+            //string y = "";
+            //for (int i = x.Length-1;i>=0 ; i--)
+            //{
+            //    y += x[i];
+            //}
+            //if (y == x)
+            //{
+            //    Console.WriteLine("perfect word");
+            //} else
+            //{
+            //    Console.WriteLine("not perfect word");
+            //}
+            #endregion
+
+
+
+        }
+    }
+}
