b5608d436376f571eb79dde80d5d465e5705dfcc
“manar <albreikimanar@gmail.com>
2026-04-22 08:18:42 +0400

    add task about converting datatype

---
+++ solution02/project01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+
+namespace project01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+//convert from string to int ----readline is string 
+            Console.WriteLine("please enter first number ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("please enter second number ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+
+            Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+
+            Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+
+            Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+
+
+
+        }
+    }
+}
